c5d917f96d1615f9a5075e45ba4ecc7e209147cb
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-06-27 18:24:23 +0300

    Deploiement de la base de donnee dans neon.tech pour faciliter la coolaboration

--- api/config.py
+++ api/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:motdepasse@localhost:5432/musicapp")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://soundwave_owner:npg_zFbkp9YfN5Dn@ep-twilight-art-a8z7occ1-pooler.eastus2.azure.neon.tech/soundwave?sslmode=require&channel_binding=require")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "SK_7D#x!Gs92kLM&+aPeD$Lz91f*!23AbQklpVXrz"
